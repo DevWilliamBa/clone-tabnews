d9389db1513200d649342d49abdba0dcd14bf276
DevWilliamBa <williamdevba@gmail.com>
2026-03-23 19:15:39 +0000

    adiciona scripts `lint:check` e `lint-flix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import { PageNotFoundError } from "next/dist/shared/lib/utils";
+
 function Home() {
-    return <h1> eu vou conseguir concluir o curso eu vou conseguirrrrrrrrr</h1> 
+  return <h1> eu vou conseguir concluir o curso eu vou conseguirrrrrrrrr</h1>;
+}
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identacacao errada");
 }
 
 export default Home;
